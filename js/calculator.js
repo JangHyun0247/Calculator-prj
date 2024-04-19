ec2c3e60e0d1dbcc5bc0abf8efbcc9ed798f4d32
yugi8 <yugi82828@naver.com>
2024-04-19 21:01:59 +0900

    사직연산 제외

--- js/calculator.js
+++ js/calculator.js
@@ -1,15 +1,22 @@
+<!-- 버튼 값 불러오기 -->
 function showValue(value) {
-    var resultElement = document.getElementById("result");
-    var text = resultElement.textContent;
+    var resultElement = document.getElementById("mainResult");  //계산화면의 값을 result에 저장
+    var text = resultElement.textContent;                       // result 의 내용을 text 변수에 저장
+
+
+
+    <!-- 숫자 버튼 눌렀을 때 결과값에 누르는것만큼 나열하기 -->
     if (text === "0") {
-        resultElement.textContent = value;
+        resultElement.textContent = value;         // 계산 화면의 값이 0이면 누르는 숫자 버튼의 숫자 값을 가져오기
     } else {
-        resultElement.textContent += value;
+        resultElement.textContent += value;        // 입력된 숫자 버튼이 있다면 그 값 뒤에 누른 숫자 버튼 나열 (십의 자리 이상)
     }
 }
 
+<!-- 클리어 버튼 -->
 function resetValue() {
-    var resultElement = document.getElementById("result");
+    var resultElement = document.getElementById("mainResult");  // 클리어 버튼 클릭시
+                                                                // 계산화면 값을 불러와 result 에 저장 후 내용 0으로 초기화
     resultElement.textContent = "0";
 }
 
